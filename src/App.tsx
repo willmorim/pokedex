9e944a695e5e94fd9713ae02523b418ec63bdb1e
willmorim <pdbawilliam@gmail.com>
2020-08-26 00:53:26 -0300

    criação da Dashboard e instalação do styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 
-const App: React.FC = () => <View />;
+const App: React.FC = () => (
+  <>
+    <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+    <View style={{ flex: 1, backgroundColor: '#fff' }} />
+  </>
+);
 
 export default App;
